c1af2890914d7b76a291ae34661a0220fc13f6bf
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-09-28 16:15:05 +0200

    chore(declarations): add "ready" message

--- web/messages.d.ts
+++ web/messages.d.ts
@@ -1,8 +1,24 @@
+// "join"
 type BattleID = string;
+
+// "ready"
+interface Position {
+  x: number;
+  y: number;
+  orientation: 'H' | 'V';
+}
+
+type ShipType = string;
+
+interface Ship {
+  position: Position,
+  type: ShipType,
+}
 
 type MessageTypes = {
   "create": null,
   "join": BattleID,
+  "ready": Ship[],
 }
 
 interface Message<K extends keyof MessageTypes> {
