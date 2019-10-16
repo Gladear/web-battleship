b8923271523ef42391e9ac902072af1a17abf42f
LucasPhilippe25 <47518756+LucasPhilippe25@users.noreply.github.com>
2019-10-16 15:36:23 +0200

    Update play.js
    update with function and listener for paramaters

--- web/src/controler/play.js
+++ web/src/controler/play.js
@@ -8,7 +8,35 @@ if (!gameID) {
 }
 
 send({ action: 'join', payload: gameID }).then(play, console.error);
+document.forms.setting.addEventListener('onsubmit', SendParameters);  
+
 
 function play() {
   // TODO Do things
 }
+
+function SendParameters (){
+
+var size = documentGetElementbyID('size').value;
+var number_Battleship = documentGetElementbyID('Battleship').value;
+var number_Cruiser = documentGetElementbyID('Cruiser').value;
+var number_Submarine = documentGetElementbyID('Submarine').value;
+var number_Patrol = documentGetElementbyID('Patrol').value;
+
+send({
+    action: 'parameters',
+    payload: {
+        width: size
+        height: size
+        ships: [
+            { name: 'Battleship', length: 4, count: number_Battleship }, 
+            { name: 'Cruiser', length: 3, count: number_Cruiser },
+            { name: 'Submarine', length: 3, count: number_Submarine },
+            { name: 'Patrol Boat', length: 2, count: number_Patrol },
+        ],
+    },
+});
+
+}
+
+
